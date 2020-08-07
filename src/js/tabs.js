253d4344e6e41f5d388e75fbafa3de0d696d5658
Jesús M. Velasco <59353677+jesusrobot@users.noreply.github.com>
2020-08-06 19:00:24 -0600

    Multi tabs bug fix

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -1,14 +1,18 @@
 (function(d) {
-  let tabs = Array.from(d.querySelectorAll('.p-tab'));
-  let panels = Array.from(d.querySelectorAll('.p-panel'));
-  d.querySelector('.p-tabs').addEventListener('click', e => {
-    if(e.target.classList.contains('p-tab')) {
-      let i = tabs.indexOf(e.target);
-      console.log(i);
-      tabs.map(tab => tab.classList.remove('p-is-active'));
-      tabs[i].classList.add('p-is-active');
-      panels.map(panel => panel.classList.remove('p-is-active'));
-      panels[i].classList.add('p-is-active');
-    }
-  })
-})(document)
+      d.addEventListener('click', e => {
+        let idTabs = Array.from(document.querySelectorAll('.p-tabs-container'));
+        let identifier = e.target.parentElement.parentElement.id;
+    
+        idTabs.forEach(element => {
+          if(element.id === identifier) {
+            let tabs = Array.from(element.children[0].children);
+            let panels = Array.from(element.children[1].children);
+            let i = tabs.indexOf(e.target);
+            tabs.map(tab => tab.classList.remove('p-is-active'));
+            tabs[i].classList.add('p-is-active');
+            panels.map(panel => panel.classList.remove('p-is-active'));
+            panels[i].classList.add('p-is-active');
+          }
+        })
+      })
+    })(document);
