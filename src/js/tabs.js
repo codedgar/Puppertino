138ae7fcb2bc90f3252043d679ac79848c220f6a
codedgar <codedgar@users.noreply.github.com>
2020-08-08 15:10:09 +0000

    Prettified Code!

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -1,18 +1,18 @@
-(function(d) {
-      d.addEventListener('click', e => {
-        let idTabs = Array.from(document.querySelectorAll('.p-tabs-container'));
-        let identifier = e.target.parentElement.parentElement.id;
-    
-        idTabs.forEach(element => {
-          if(element.id === identifier) {
-            let tabs = Array.from(element.children[0].children);
-            let panels = Array.from(element.children[1].children);
-            let i = tabs.indexOf(e.target);
-            tabs.map(tab => tab.classList.remove('p-is-active'));
-            tabs[i].classList.add('p-is-active');
-            panels.map(panel => panel.classList.remove('p-is-active'));
-            panels[i].classList.add('p-is-active');
-          }
-        })
-      })
-    })(document);
+(function (d) {
+  d.addEventListener("click", (e) => {
+    let idTabs = Array.from(document.querySelectorAll(".p-tabs-container"));
+    let identifier = e.target.parentElement.parentElement.id;
+
+    idTabs.forEach((element) => {
+      if (element.id === identifier) {
+        let tabs = Array.from(element.children[0].children);
+        let panels = Array.from(element.children[1].children);
+        let i = tabs.indexOf(e.target);
+        tabs.map((tab) => tab.classList.remove("p-is-active"));
+        tabs[i].classList.add("p-is-active");
+        panels.map((panel) => panel.classList.remove("p-is-active"));
+        panels[i].classList.add("p-is-active");
+      }
+    });
+  });
+})(document);
