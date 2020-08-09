03dd26e200d61ba802501c78767ca827426d72f7
Stan <stanip@hotmail.de>
2020-08-09 18:02:32 +0200

    feat: created a monorepo

---
+++ packages/puppertino-css/src/js/segmented_controls.js
@@ -0,0 +1,12 @@
+var seg_controll = (function (document) {
+  var p_segmented_controls = document.querySelectorAll(".p-segmented-controls a");
+  for (var item of p_segmented_controls) {
+    item.addEventListener("click", function (event) {
+      event.preventDefault();
+      this.parentElement.querySelector("a.active").classList.remove("active");
+      this.classList.add("active");
+    });
+  }
+})(document)
+
+export default seg_controll
