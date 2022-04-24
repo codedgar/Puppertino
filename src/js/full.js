c51b64c41e4c4312d1c42e83eeb59f69c23b2222
Pronoy Mukherjee <83197609+pronoy2108@users.noreply.github.com>
2022-04-24 16:42:05 +0530

    Create full.js

---
+++ src/js/full.js
@@ -0,0 +1,8 @@
+<script type="text/javascript" src="actions.js">
+</script>
+<script type="text/javascript" src="modals.js">
+</script>
+<script type="text/javascript" src="segmented_controls.js">
+</script>
+<script type="text/javascript" src="tabs.js">
+</script>
