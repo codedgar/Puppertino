573c2d36a4c7606ab06a62fa94b36a42fea6e6a6
Edgar <edgar.ramon23@gmail.com>
2022-05-10 11:28:21 -0400

    Changed homepage, making changes into docs

---
+++ src/js/Puppertino.js
@@ -0,0 +1,33 @@
+function Puppertino(options, selector){
+    return {
+        options: options,
+        selector: selector,
+        modal: function(selector){
+            console.log('AAAAAAAA');
+
+            this.init = function(){
+                console.log('huh');
+            }
+            this.show = function(selector){
+                console.log(document.querySelector(selector));
+            }
+            this.hide = function(selector){
+
+                console.log(document.querySelector(selector));
+            }
+            this.toggle = function(selector){
+                console.log(document.querySelector(selector));
+            }
+            return this
+        }
+    }
+}
+
+options = {
+    'modal': {
+        'selector': 'try'
+    }
+}
+
+const robo1 = Puppertino().modal().show('body');
+
