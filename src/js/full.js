fba7fffe03ee4194d5e2c06de198212438dc0e73
Sofia Vicedomini <11537307+blacksoulgem95@users.noreply.github.com>
2024-12-13 20:09:33 +0100

    Revert "FEATURE: Automated build"
    This reverts commit a2434abb6794fe81a87eb6925e9e3ab7a28a94e6.

---
+++ src/js/full.js
@@ -0,0 +1,4 @@
+import('actions.js')
+import('modals.js')
+import('segmented_controls.js')
+import('tabs.js')
--- src/js/index.umd.js
+++
@@ -1,5 +0,0 @@
-import('./actions')
-import('./darkmode_manager')
-import('./modals')
-import('./segmented_controls')
-import('./tabs')
